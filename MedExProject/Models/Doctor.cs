cecaa365eeeb45317f0db7ec5a8758e8432efa17
Alex <alex.spruill95@gmail.com>
2017-11-08 16:43:20 -0700

    Added a table and models?

---
+++ MedExProject/Models/Doctor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MedExProject.Models
+{
+    [Table("Doctor")]
+    public class Doctor
+    {
+        [Key]
+        public int DoctorID { get; set; }
+        public string DoctorFirstName { get; set; }
+        public string DoctorLastName { get; set; }
+        public string DoctorPhone { get; set; }
+        [EmailAddress]
+        public string DoctorEmail { get; set; }
+
+    }
+}
