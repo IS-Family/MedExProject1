1049b1f3f52d1eba930843bb632b7f0de6b8742a
Alex <alex.spruill95@gmail.com>
2017-11-08 17:12:26 -0700

    Entity Framework

---
+++ MedExProject/DAL/MedEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MedExProject.Models;
+using System.Data.Entity;
+
+namespace MedExProject.DAL
+{
+    public class MedEx : DbContext
+    {
+        public MedEx() : base("MedEx")
+        {
+
+        }
+
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<DocExpirationDates> DocExpirationDatesEach { get; set; }
+    }
+}
