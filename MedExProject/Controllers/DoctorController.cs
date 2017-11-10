3eb93c680cbe2a0e92b99807f09b2b9b8475cef0
Matthew Kastner <matthewkastner@gmail.com>
2017-11-10 15:11:32 -0700

    controllers

---
+++ MedExProject/Controllers/DoctorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedExProject.Controllers
+{
+    public class DoctorController : Controller
+    {
+        // GET: Doctor
+        public ActionResult Personal()
+        {
+            return View();
+        }
+
+        // GET: Doctor
+        public ActionResult License()
+        {
+            return View();
+        }
+    }
+}
