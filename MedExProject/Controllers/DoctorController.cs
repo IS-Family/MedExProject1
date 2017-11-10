2bfedbf0d3d343bcc6e879dad3be4d6616ea49fd
Matthew Kastner <matthewkastner@gmail.com>
2017-11-10 15:59:07 -0700

    personal

--- MedExProject/Controllers/DoctorController.cs
+++ MedExProject/Controllers/DoctorController.cs
@@ -14,6 +14,16 @@ namespace MedExProject.Controllers
             return View();
         }
 
+        [HttpPost]
+        public String enterData(FormCollection form)
+        {
+            String fName = form["Email address"].ToString();
+            String lName = form["Email address"].ToString();
+            String  phoneNumber = form["Email address"].ToString();
+            String email = form["Email address"].ToString();
+            return "Signing in : " + email;
+        }
+
         // GET: license information
         public ActionResult License()
         {
